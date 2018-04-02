7fdb078b0a1f213abdf827354484f8360236aa63
Ben Hundley <ben.hundley@gmail.com>
2018-04-02 13:48:46 -0500

    WIP

--- modules/openstack/etcd/output.tf
+++ modules/openstack/etcd/output.tf
@@ -1,3 +1,14 @@
 output "user_data" {
   value = "${data.ignition_config.etcd.*.rendered}"
 }
+
+locals {
+  k8s_endpoint_address_format = "{ip: %s, nodeName: %s}"
+}
+
+output "k8s_endpoint_addresses" {
+  value = "[${join(", ",
+    formatlist(local.k8s_endpoint_address_format,
+    var.etcd_ip_addresses,
+    var.etcd_ip_addresses))}]"
+}
